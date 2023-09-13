ca1fd60bf62b547cfa8d277be7e12c029443cf6b
Thiago <thiago2b9s@gmail.com>
2023-09-13 03:54:27 -0300

    commit pra mostrar que o trabalho é AUTORAL

---
+++ Atv02/TP02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Se você me plagiar denovo henrique eu juro que te mato
+//Autoria de Thiago da Silva
+
+
+//Função de troca de valor entre varíaveis utilizando ponteiros para strings
+void swap (char **a, char **b){
+    char *temp = *a;
+    *a = *b;
+    *b = *temp;
+}
+
+
+int main() {
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+<h1> AUTORIA DE THIAGO DA SILVA<br>
+CÓPIA/PLÁGIO NÃO AUTORIZADO</h1>
+<h2> Repositório de Estrutura de dados da UCB</h2>
+<strong>Tudo neste repositório é autoral feito com auxílio de fontes citadas na documentação de cada atividade</strong>
+<br>
+
+# BlackList
+- Henrique Serafin
